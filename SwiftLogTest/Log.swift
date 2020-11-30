e3a01ee47691c784523d9f52c0cd2749e51af5fb
Oleg Misal <v-olmisa@microsoft.com>
2020-11-30 14:03:23 +0300

    Init commit

---
+++ SwiftLogTest/Log.swift
@@ -0,0 +1,18 @@
+//
+//  Log.swift
+//  SwiftLogTest
+//
+//  Created by OlegMisal on 30.11.2020.
+//
+
+import Foundation
+
+class Log : NSObject {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
---
+++ SwiftLogTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftLogTest
+//
+//  Created by OlegMisal on 30.11.2020.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
